058e23d26ad2167770836757eee4461b6b93a982
Iqra Masroor <masrooriqra@gmail.com>
2019-12-14 18:44:17 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,9 +22,6 @@ def get_japanese_emoticon(file_path, emoticon)
   else
     "Sorry, that emoticon was not found"
   end
-  #return library[:get_emoticon][emoticon]
-  #else return "Sorry, that emoticon was not found"
-  # "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning(file_path, emoticon)
