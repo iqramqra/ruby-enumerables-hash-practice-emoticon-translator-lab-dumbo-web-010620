5075bdf7eb6212c990b9765b32a2f33878479e1c
Iqra Masroor <masrooriqra@gmail.com>
2019-12-14 18:36:04 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,7 +18,7 @@ def get_japanese_emoticon(file_path, emoticon)
   # code goes here
   library=load_library(file_path)
   # return emo_result["get_emoticon"][eng_emote]
-  return library[:get_emoticon][]
+  return library[:get_emoticon][emoticon]
  #library[:get_emoticon].each do |key, value|
     if key==emoticon
       puts value
